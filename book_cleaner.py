ab28124526bed6881564a1c2fc11444d6f59c358
Joshua Stephenson <josh@brightmediums.com>
2023-11-28 07:46:06 -0500

    initial commit, working book_parser and topic modeleler (gts.py)

---
+++ book_cleaner.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import subprocess
+import readchar
+import os.path
+from gts import Book
+from pathlib import Path
+import re
+import pickle
+
+path = 'PG'
+files = [str(child.resolve()) for child in sorted(Path.iterdir(Path(path)))]
+files = [file for file in files if file.endswith('.txt')]
+
+log = '.book-cleaner-log'
+pickle_path = Path(log).resolve()
+print(pickle_path)
+ignored = []
+if os.path.exists(pickle_path):
+    file = open(pickle_path, 'rb')
+    ignored = pickle.load(file)
+    file.close()
+
+def path_for(book):
+    def valid_char(char):
+        return char.isalnum() or '_' == char
+    title = re.sub(r'\s', '_', book.title)
+    title = ''.join(e for e in title if valid_char(e))
+    author = re.sub(r'\s', '_', book.author)
+    author = ''.join(e for e in author if valid_char(e))
+    filename = f'{title}-by-{author}.txt'
+    path = Path(f'Books/{filename}').resolve()
+    return path
+
+def file_exists(path):
+    return os.path.exists(path)
+
+def save_clean_file(book, path):
+    with open(path, 'w') as f:
+        f.write('\n'.join(book.body_lines))
+    f.close()
+    subprocess.run(['vim', path])
+
+def ignore(key):
+    ignored.append(key)
+    file = open(pickle_path, 'wb')
+    pickle.dump(ignored, file)
+    file.close()
+
+for file in files:
+    book = Book(file)
+    if book.has_body():
+        path = path_for(book)
+        if not file_exists(path) and path not in ignored:
+            print(f'Would you like to save "{book.title}" by "{book.author}"? ')
+            inp = readchar.readchar()
+            if inp == '\n':
+                save_clean_file(book, path)
+            else:
+                ignore(path)
+        os.remove(file)
---
+++ gts.py
@@ -0,0 +1,336 @@
+#!/usr/bin/env python3
+
+from enum import IntEnum
+import copy
+import re
+import os.path
+from pathlib import Path
+import nltk
+from nltk import word_tokenize, sent_tokenize, pos_tag, FreqDist, ngrams
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tokenize import MWETokenizer
+from nltk.lm.preprocessing import flatten
+import json
+import gensim
+from gensim.corpora import Dictionary
+from gensim.models import CoherenceModel, Phrases, LdaModel
+from gensim.utils import tokenize
+from gensim.parsing.preprocessing import preprocess_string, remove_stopwords, stem_text
+import pyLDAvis
+import pyLDAvis.gensim
+#import spacy
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import pickle
+
+def setup_mwes():
+    mwe_body_lines = []
+    with open('mwelex/wordnet_mwes.json', 'r') as f:
+        mwe_lines = f.readlines()
+    f.close()
+    mwes = []
+    mwe_lookup = {}
+    for line in mwe_lines:
+        js = json.loads(line)
+        lemma = js['wnlemmas']
+        words = js['words']
+        key = list(lemma.keys())[0]
+        supersense = lemma[key]['supersense'].strip()
+        mwes.append(tuple(words))
+        lookup_key = '_'.join(words)
+        pos = supersense.split('.')[0].strip()
+        mwe_lookup[lookup_key] = {'supersense': supersense, 'POS_GROUP': pos}
+
+    return MWETokenizer(mwes)
+
+stop = stopwords.words('english')
+stop.extend(['thou', 'thing', 'hath', 'part', 'ye', 'thee', 'thy', 'ere', 'nay', 'hast', 'ha', 'wilt', 'thereafter', '....', 'en', 'dat', 'ai', 'gwine', 'aye', 'quoth', 'beheld', 'behold', 'thine', 'unto', 'chapter', 'book', 'th', 'yo', 'wi', 'de', 'yet'])
+
+def roman_numeral(number):
+    num = [1, 4, 5, 9, 10, 40, 50, 90,
+        100, 400, 500, 900, 1000]
+    sym = ["I", "IV", "V", "IX", "X", "XL",
+        "L", "XC", "C", "CD", "D", "CM", "M"]
+    i = 12
+
+    numeral = ""
+    while number:
+        div = number // num[i]
+        number %= num[i]
+
+        while div:
+            numeral += sym[i]
+#            print(sym[i], end = "")
+            div -= 1
+        i -= 1
+    return numeral
+
+numerals = [roman_numeral(i).lower() for i in range(100)]
+stop.extend(numerals)
+#nlp = spacy.load('en_core_web_sm', disable = ['parser', 'ner'])
+#nlp.max_length = 100000000
+lemmatizer = WordNetLemmatizer()
+
+POS = ['NOUN']
+TITLE_RE = r"^Title: "
+AUTHOR_RE = r"^Author: "
+START_RE = r'\*+\s*START OF'
+END_RE = r'\*+\s*END OF'
+PARSE_MWES = False
+SUMMARIZE = False
+TARGET_WORDCOUNT = 1000
+
+if PARSE_MWES:
+    mwe_tokenizer = setup_mwes()
+
+class MethodStep(IntEnum):
+    basic = 0
+    trimwords = 1
+    stopwords = 2
+    onlynouns = 3
+    lemmatize = 4
+    bigrams = 5
+
+METHOD = MethodStep.basic
+class Book:
+
+    @classmethod
+    def load(self, dirname = 'PG', matching = None, cleaned = False):
+        path = dirname
+        files = [str(child.resolve()) for child in sorted(Path.iterdir(Path(path)))]
+        if matching:
+            files = [file for file in files if file.endswith('.txt') and matching in file]
+        else:
+            files = [file for file in files if file.endswith('.txt')]
+
+        books  = [Book(f, cleaned) for f in files]
+        valid = [b for b in books if b.has_body()]
+        print(f'{len(valid)} valid books out of total {len(books)}')
+
+        return valid
+
+    def __init__(self, file, cleaned = False):
+        self.title = ''
+        self.author = ''
+        self.file = file
+        self.body = ''
+        self.body_lines = []
+        self.lemma = []
+        self.process(cleaned)
+
+    def has_body(self):
+        return bool(len(self.body_lines))
+
+    def process(self, cleaned):
+        def is_word(word):
+            word = word.lower()
+            return word.isalpha() and word not in stop
+
+        def is_noun(word):
+            word = word.lower()
+            if not word.isalpha() or word in stop:
+                return False
+            tag = pos_tag([word])
+            pos = tag[0][1]
+            return pos.startswith('NN') and pos != 'NNP' # no proper nouns
+
+        def _tokenize(body, trim = False, only_nouns = False):
+            if trim:
+                # We only want first 1000 words trimmed at nearest sentence.
+                sentences = sent_tokenize(body)
+                count = 0
+                words = []
+                for sent in sentences:
+                    if only_nouns:
+                        # don't lowercase nouns, we need case for proper noun detection
+                        w = [word for word in list(tokenize(sent, lower = False)) if is_noun(word)]
+                    else:
+                        w = list(tokenize(sent, lower = False))
+                    count += len(w)
+                    words = words + w
+                    if count >= TARGET_WORDCOUNT:
+                        break
+                return words
+            else:
+                return [w.lower() for w in word_tokenize(body) if w.isalpha()]
+
+        def _remove_stopwords(words):
+            return [word for word in words if is_word(word)]
+
+        def remove_all_but_nouns(body):
+            return _tokenize(body, trim = True, only_nouns = True)
+
+        def lemmatize(body):
+            words = remove_all_but_nouns(body)
+            return [lemmatizer.lemmatize(word) for word in words]
+
+        def bigrams(body):
+            # Add bigrams
+            sentences = sent_tokenize(body.replace('_', '').replace('\n', ''))
+            sentences = [list(tokenize(sent, lower = False)) for sent in sentences]
+            sentences = [[lemmatizer.lemmatize(word) for word in sent if is_noun(word)] for sent in sentences]
+            model = Phrases(sentences, min_count = 3, threshold = 2)
+            tokens = [preprocess_string(" ".join(sent), []) for sent in sentences]
+            bigrams = model[tokens]
+            rtn = []
+            for b in bigrams:
+                for w in b:
+                    if '_' in w:
+                        rtn.append(w)
+            return rtn
+
+        self.parse_book(cleaned)
+
+        if self.has_body():
+            print(f'Parsed book: {self.title} by {self.author} w/ {len(self.body_lines)} lines of text.')
+
+            self.lemma = _tokenize(self.body, METHOD >= MethodStep.trimwords)
+            old_lemma = len(self.lemma)
+            match METHOD:
+                case MethodStep.basic:
+                    pass
+                    # nothing, handled above
+                case MethodStep.trimwords:
+                    self.lemma = _tokenize(self.body, True)
+                case MethodStep.stopwords:
+                    self.lemma = _remove_stopwords(self.lemma)
+                    assert old_lemma != len(self.lemma)
+                case MethodStep.onlynouns:
+                    self.lemma = remove_all_but_nouns(self.body)
+                    assert old_lemma != len(self.lemma)
+                case MethodStep.lemmatize:
+                    self.lemma = lemmatize(self.body)
+                    assert old_lemma != len(self.lemma)
+                case MethodStep.bigrams:
+                    self.lemma = bigrams(self.body)
+                    assert old_lemma != len(self.lemma)
+
+        return self
+
+    def __str__(self):
+        return f'{self.title} ({len(self.body)}) by {self.author}'
+
+    def parse_book(self, cleaned = False):
+        if cleaned:
+            filename = self.file.split('/')[-1]
+            parts = filename.replace('.txt', '').split('-by-')
+            title = parts[0].replace('_', ' ')
+            author = parts[1].replace('_', ' ')
+            self.title = title
+            self.author = author
+            with open(self.file, 'r', encoding = 'latin') as f:
+                for line in f.readlines():
+                    self.body_lines.append(line)
+        else:
+            is_reading = False
+            found_title = False
+            found_author = False
+            with open(self.file, 'r', encoding = 'latin') as f:
+                for line in f.readlines():
+                    if not found_title and re.search(TITLE_RE, line):
+                        self.title = re.sub(TITLE_RE, "", line).strip()
+                        found_title = True
+
+                    if not found_author and re.search(AUTHOR_RE, line):
+                        self.author = re.sub(AUTHOR_RE, "", line).strip()
+                        found_author = True
+
+                    # Don't save header content which is a lot of messaging from Gutenberg
+                    if not is_reading and re.search(START_RE, line):
+                        is_reading = True
+                        continue # skip this line
+
+                    # Don't save footer content either
+                    elif is_reading and re.search(END_RE, line):
+                        is_reading = False
+
+                    line = line.strip()
+                    if is_reading and len(line) > 0:
+                        self.body_lines.append(line)
+
+        f.close()
+        self.body = ' '.join(self.body_lines)
+
+FIND_COHERENCE = False
+NUM_TOPICS = 20
+if __name__ == '__main__':
+    def compute_coherence_values(dictionary, corpus, texts, nums):
+        coherence_values = []
+        model_list = []
+        for num_topics in nums:
+            print(f'num_topics: {num_topics}')
+            model = gensim.models.LdaModel(corpus=corpus, id2word=id2word, num_topics=num_topics)
+            model_list.append(model)
+            coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
+            coherence_values.append(coherencemodel.get_coherence())
+
+        return model_list, coherence_values
+
+    def run_analysis(start = 2, limit = 41, step = 3):
+        x = list(range(start, limit, step))
+        model_list, coherence_values = compute_coherence_values(dictionary = id2word, corpus = corpus, texts = all_lemma, nums = x)
+        print(coherence_values)
+        plt.figure()
+        plt.plot(x, coherence_values)
+        plt.xlabel("Num Topics")
+        plt.ylabel("Coherence score")
+        plt.legend(("coherence_values"), loc='best')
+        plt.savefig('coherence.png')
+
+    method_str = str(METHOD).split('.')[1]
+    cache_name = f'books-{NUM_TOPICS}-{method_str}.pkl'
+    if os.path.isfile(cache_name):
+        print('already have books cached')
+        pickle_file = open(cache_name, 'rb')
+        books = pickle.load(pickle_file)
+        pickle_file.close()
+        print(f'Loaded {len(books)} cached books')
+    else:
+        books = Book.load('Books', matching = None, cleaned = True)
+        pickle_file = open(cache_name, 'wb')
+        pickle.dump(books, pickle_file)
+        pickle_file.close()
+
+
+    if FIND_COHERENCE:
+        run_analysis(2, 41, 3)
+    else:
+        docs = [book.lemma for book in books]
+
+        dictionary = Dictionary(docs)
+        dictionary.filter_extremes(no_below = 3, no_above = 0.5)
+        corpus = [dictionary.doc2bow(doc) for doc in docs]
+        temp = dictionary[0] # prime the dictionary ???
+        id2word = dictionary.id2token
+
+        model = LdaModel(corpus = corpus,
+            id2word = id2word,
+            chunksize = 20,
+            alpha = 'auto',
+            eta = 'auto',
+            iterations = 50,
+            num_topics = NUM_TOPICS,
+            passes = 20,
+            eval_every = None)
+
+        top_topics = model.top_topics(corpus)
+
+        prepared = pyLDAvis.gensim.prepare(model, corpus, dictionary, mds = 'mmds')
+        filename = './books-'+ str(NUM_TOPICS) + '-' + method_str + '.html'
+        print(filename)
+        pyLDAvis.save_html(prepared, filename)
+
+        print("Making word clouds")
+        for i in range(NUM_TOPICS):
+            w = WordCloud().fit_words(dict(model.show_topic(i, 50)))
+            plt.imshow(w)
+            plt.savefig('book-wordcloud-' + method_str +'-topic-' + str(i) + '.png')
+            plt.axis("off")
+
+        print(f'Finding topics for first 10 books')
+        for b in books:
+            print(b.title)
+            topics = model.get_document_topics(dictionary.doc2bow(b.lemma))
+            ids = [a for a,b in topics]
+            print(model.show_topic(ids[0]))
